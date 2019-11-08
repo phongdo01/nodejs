c3dd174b3d3a59183b5ac6850be04429e4e3bb8d
Phong <dovanphong96@gmail.com>
2019-11-08 07:26:07 +0700

    cmt

--- apps/routers/signin.js
+++ apps/routers/signin.js
@@ -2,21 +2,25 @@ const app = require('express')
 const router = app.Router()
 const con = require('../config/connection').connection
 const saySomething = require('../config/connection').saySomething
-router.get('/', function(req, res){
-    res.render('signin')
+router.get('/', function (req, res) {
+    res.render('signin', {message:{}})
 })
-router.post('/', function(req, res){
-    con.connect(function(err){
-        if (err) throw err
-        const data = req.body
-        res.json(data)
-        var sql = "INSERT INTO account (username, password) VALUES ?";
-        const value = [[data.username, data.password]]
-        con.query(sql, [value], function(err, result){
-            if (err) throw err;
-            console.log('record effect: ', result.affectedRows)
+router.post('/', function (req, res) {
+    const data = req.body
+    console.log('data: ', data.username.length)
+    if (data.username.length == 0) {
+        res.render('signin', { message: { err: "User name can not be null" } })
+    }
+    else
+        con.connect(function (err) {
+            if (err) throw err
+            res.json(data)
+            var sql = "INSERT INTO account (username, password) VALUES ?";
+            const value = [[data.username, data.password]]
+            con.query(sql, [value], function (err, result) {
+                if (err) throw err;
+                console.log('record effect: ', result.affectedRows)
+            })
         })
-    })
 })
-saySomething()
 module.exports = router
