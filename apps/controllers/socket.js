c60818c99d86d242a7a2a2dce6f0a38109fadec4
Phong <dovanphong96@gmail.com>
2019-11-14 23:53:14 +0700

    connect socketio

--- apps/controllers/socket.js
+++ apps/controllers/socket.js
@@ -1,5 +1,8 @@
 module.exports = function (io) {
     io.sockets.on('connection', function (socket) {
         console.log('a user connected');
+        socket.on("getUserName", function(data){
+            socket.broadcast.emit("newUser", data.userName)
+        })
     });
 }
